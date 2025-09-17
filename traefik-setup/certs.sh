926c64695caf7315e5aa512310023335e65d049a
Muritala David <davmuri1414@gmail.com>
2025-09-17 12:05:10 +0100

    traefik setup

--- traefik-setup/certs.sh
+++
@@ -1,7 +0,0 @@
-mkdir certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout certs/x.brimble.app.key -out certs/x.brimble.app.crt \
-  -subj "/CN=x.brimble.app"
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout certs/y.brimble.app.key -out certs/y.brimble.app.crt \
-  -subj "/CN=y.brimble.app"
